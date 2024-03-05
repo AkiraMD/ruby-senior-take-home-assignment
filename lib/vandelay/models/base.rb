9427ea0a939071e3bd33322ba0470a10651550b9
Hernan Velasquez <hernamvel@gmail.com>
2024-03-05 14:02:06 -0800

    better aproach for bug fix

--- lib/vandelay/models/base.rb
+++ lib/vandelay/models/base.rb
@@ -9,7 +9,7 @@ module Vandelay
         data.each do |prop, val|
           self.instance_variable_set "@#{prop}", val
           self.class.class_eval do
-            define_method("#{prop}") { eval "@#{prop}" }
+            attr_accessor prop
           end
         end
       end
